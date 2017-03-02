0ac6bb6166006fba354381253990dc0ee3bcb25a
Humble <humble_zh@163.com>
2017-03-02 21:56:55 +0800

    leds字符设备驱动和应用_程序框架_v0.0.2

--- frame0/app/app.c
+++ frame0/app/app.c
@@ -31,6 +31,9 @@ int main(int argc,char **argv)
 		return 0;
 	}
 	filename = argv[1];
+	/* open(),wirte()函数是由C库实现的，实际上执行了一条swi val指令引发异常（中断），跳到内
+	   核异常处理函数（系统调用接口）。然后系统调用接口根据发生中断原因(val)调用不同处理函数。
+	*/
 	fd = open(filename,O_RDWR);
 	if (fd < 0){
 		printf("ERROR: %s open failed.\n",filename);
@@ -56,6 +59,12 @@ void usage(void)
 #define USAGE "\
 Usage:\n\
   Please use command like this : \n\
-    ./app /dev/led[1-3|s] [on|off]\n"
+    ./app /dev/led<1-3|s> <on|off>\n"
 	printf(USAGE);
 }
+
+
+/*
+VFS（虚拟文件系统）：sys_open,sys_read,sys_write 根据程序打开的文件的类型或者属性进行调用相应的驱动程序，例：open("/dev/led",O_RDWR)————led_open()
+*/
+
--- frame0/drv/frame0_drv.c
+++ frame0/drv/frame0_drv.c
@@ -22,16 +22,19 @@
 #include <asm/arch/regs-gpio.h>
 #include <asm/hardware.h>
 
-#define DEVICE_NAME     "leds"
+#define DEVICE_NAME     "leds"	/* 加载模式后，执行”cat /proc/devices”命令看到的设备名称 */
 #define CLASS_NAME		"leds_c"
-//#define LED_MAJOR       231	/* 手动分配主设备号(好像无法创建类) */
+//#define LED_MAJOR       231	/* 手动分配主设备号，注意register_chrdev的调用 */
 #define LED_MAJOR       0		/* major写0，自动分配设备号 */
 
 static struct class *leds_class;
 static struct class_device *leds_class_devs[4];
 
-volatile unsigned long *GPFCON = NULL;
-volatile unsigned long *GPFDAT = NULL;
+static unsigned long gpio_va;
+
+#define GPIO_OFT(x) ((x) - 0x56000000)
+#define GPFCON  (*(volatile unsigned long *)(gpio_va + GPIO_OFT(0x56000050)))
+#define GPFDAT  (*(volatile unsigned long *)(gpio_va + GPIO_OFT(0x56000054)))
 
 int major = LED_MAJOR;
 static int leds_open(struct inode *inode,struct file *file)
@@ -41,23 +44,23 @@ static int leds_open(struct inode *inode,struct file *file)
 	printk("In \'leds_open()\' func: \n\tmajor:%d\tminior:%d\n",major,minor);
 	switch(minor){
 	case 0: /* /dev/leds 配置GPF 4 5 6 引脚为输出引脚,并输出1 灭灯 */
-		*GPFCON &= ~((3<<(4*2)) | (3<<(5*2)) | (3<<(6*2)));
-		*GPFCON |=  ((1<<(4*2)) | (1<<(5*2)) | (1<<(6*2)));
+		GPFCON &= ~((3<<(4*2)) | (3<<(5*2)) | (3<<(6*2)));
+		GPFCON |=  ((1<<(4*2)) | (1<<(5*2)) | (1<<(6*2)));
 		break;
 	case 1: /* /dev/led1 配置GPF 4 引脚为输出引脚,并输出1 灭灯 */
-		*GPFCON &= ~(3<<(4*2));
-		*GPFCON |=  (1<<(4*2));
+		GPFCON &= ~(3<<(4*2));
+		GPFCON |=  (1<<(4*2));
 		break;
 	case 2:/*  /dev/led2 配置GPF 5 引脚为输出引脚,并输出1 灭灯 */
-		*GPFCON &= ~(3<<(5*2));
-		*GPFCON |=  (1<<(5*2));
+		GPFCON &= ~(3<<(5*2));
+		GPFCON |=  (1<<(5*2));
 		break;
 	case 3:/*  /dev/led3 配置GPF 6 引脚为输出引脚,并输出1 灭灯 */
-		*GPFCON &= ~(3<<(6*2));
-		*GPFCON |=  (1<<(6*2));
+		GPFCON &= ~(3<<(6*2));
+		GPFCON |=  (1<<(6*2));
 		break;
 	}
-	printk("Register GPFCON: 0x%x\n",*GPFCON);
+	printk("Register GPFCON: 0x%x\n", (int)&GPFCON);
 	return 0;
 }
 
@@ -72,30 +75,30 @@ static ssize_t leds_write(struct file *file,const char __user *buf,size_t count,
 	switch(minor){
 	case 0: // /dev/leds
 		if(0==val) //点灯
-			*GPFDAT &= ~((1<<4) | (1<<5) | (1<<6));
+			GPFDAT &= ~((1<<4) | (1<<5) | (1<<6));
 		else		   //灭灯
-			*GPFDAT |= (1<<4) | (1<<5) | (1<<6); 
+			GPFDAT |= (1<<4) | (1<<5) | (1<<6); 
 		break;
 	case 1: // /dev/led1
 		if(0==val) //点灯
-			*GPFDAT &= ~(1<<4);
+			GPFDAT &= ~(1<<4);
 		else		   //灭灯
-			*GPFDAT |= (1<<4); 
+			GPFDAT |= (1<<4); 
 		break;
 	case 2: // /dev/led1 
 		if(0==val) //点灯
-			*GPFDAT &= ~(1<<5);
+			GPFDAT &= ~(1<<5);
 		else		   //灭灯
-			*GPFDAT |= (1<<5); 
+			GPFDAT |= (1<<5); 
 		break;
 	case 3: // /dev/led1
 		if(0==val) //点灯
-			*GPFDAT &= ~(1<<6);
+			GPFDAT &= ~(1<<6);
 		else		   //灭灯
-			*GPFDAT |= (1<<6); 
+			GPFDAT |= (1<<6); 
 		break;
 	}
-	printk("Register GPFDAT : 0x%x\n",*GPFDAT);
+	printk("Register GPFDAT : 0x%x\n",(int)&GPFDAT);
 	return 0;
 }
 
@@ -109,18 +112,28 @@ static struct file_operations leds_fops = {
 //int major;
 int leds_init(void)
 {
+	int minor = 0;
 	//a.IO地址重映射
 	//ioremap(起始物理地址，映射长度是多少字节)
-	GPFCON = (volatile unsigned long *)ioremap(0x56000050,16);
-	GPFDAT = GPFCON + 1;
+	gpio_va = ioremap(0x56000000,16);
+	if (!gpio_va) {
+		return -EIO;
+	}
 	
 	//1.注册结构体(函数接口)
-	//把结构体leds_fops注册到数组chardev[]里面的第major项里面。
+	//把结构体leds_fops注册到数组chardev[]里面的第major项里面。如果register_chrdev第一个参数非0，创建成功就返回0，此时不应该把返回值赋给major
 	major = register_chrdev(major,DEVICE_NAME,&leds_fops);
-	int minor = 0;
+	printk("major:%u\n", major);
+	if (major < 0) {
+		printk(DEVICE_NAME " can't register major number\n");
+		return major;
+    }
 	
-	//2.在/sys目录下创建类：leds
+	//2.在/sys目录下创建类：leds_c
 	leds_class = class_create(THIS_MODULE, CLASS_NAME);
+	if (IS_ERR(leds_class)){
+		return PTR_ERR(leds_class);
+	}
 	printk("Creat class '%s' succeed!\n", CLASS_NAME);
 	
 	//3.在类下创建设备文件(设备节点)
@@ -129,6 +142,9 @@ int leds_init(void)
 	for(minor=1;minor<4;minor++)
 	{
 		leds_class_devs[minor] = class_device_create(leds_class,NULL,MKDEV(major,minor),NULL,"led%d",minor);
+		if (unlikely(IS_ERR(leds_class_devs[minor]))){
+			return PTR_ERR(leds_class_devs[minor]);
+		}
 	}
 	printk("mknod \"/dev/leds and led1-3\" succeed!\n");
 	return 0;
@@ -153,16 +169,16 @@ void leds_exit(void)
 	printk("All leds exited!\n");
 	
 	//a.IO地址取消重映射
-	iounmap(GPFCON);
+	iounmap(gpio_va);
 }
 
 
 //module_init:定义一个结构体，结构体内部有一个函数指针指向leds_init函数的指针。当我们加载或安装一个驱动的时候，内核会找到结构体，用函数指针调用leds_init函数，这个函数就把leds_fops结构告诉内核
-module_init(leds_init);
-module_exit(leds_exit);
+module_init(leds_init);//指定执行insmod命令时调用的函数
+module_exit(leds_exit);//指定执行rmmod命令时调用的函数
 
 MODULE_AUTHOR("humble_zh");
-MODULE_VERSION("0.0.1");
+MODULE_VERSION("0.0.2");
 MODULE_DESCRIPTION("JZ2440v2 LEDs Driver");
 MODULE_LICENSE("GPL");
 
